d429e1e9a699bc2f53efac4072e83387d1849f91
HMJ Produksi Pertanian <produksipertanian.polije@gmail.com>
2024-07-11 23:23:51 +0700

    Update main.js

--- medkominfo/script/main.js
+++ medkominfo/script/main.js
@@ -1,13 +1,13 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'Music?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
+        confirmButtonText: 'Boleh',
+        cancelButtonText: 'Gk ws',
     }).then((result) => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
